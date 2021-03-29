5c647cc2d906052e8704c2923f280045525d2a70
martijn <martijn.brug@gmail.com>
2021-03-29 15:51:09 +0200

    Added huidverjongende pages

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -1,31 +1,37 @@
 <template>
   <v-app style="font-family: 'Montserrat', sans-serif; color: #1f1f1f;">
     <core-app-bar/>
-
     <core-drawer/>
-
     <div id="spacer" style="height:120px;"></div>
-
     <slot/>
 
     <br/>
     <br/>
+    <div id="footer-banner" class="text-center" style="background-color: #2F837F; padding: 5px;"><a :href="global.footerBannerLink">{{ global.footerBanner }}</a></div>
     <core-footer/>
-
   </v-app>
 </template>
-
 <script>
 import CoreCTA from '../components/core/Cta';
 import CoreDrawer from '../components/core/Drawer';
 import CoreFooter from '../components/core/Footer';
 import CoreAppBar from '../components/core/AppBar';
+const global = require('@/data/global.json');
 
 export default {
   name: 'App',
-
   components: {
     CoreCTA, CoreAppBar, CoreDrawer, CoreFooter
+  },
+  data() {
+    return {
+      global
+    }
   }
 }
 </script>
+<style>
+#footer-banner a{
+  color: white;
+}
+</style>
---
+++ src/pages/HuidverjongendeGezichtsbehandelingen.vue
@@ -0,0 +1,51 @@
+<template>
+  <Layout>
+    <section id="huidverjongende-behandeling">
+      <v-container>
+
+        <v-row>
+          <v-col
+              cols="12"
+          >
+            <br>
+            <h1>
+              {{ data.title }}
+            </h1>
+
+            <p v-html="data.body"></p>
+          </v-col>
+        </v-row>
+        <v-row>
+          <feed-card
+              v-for="block in data.blocks"
+              :key="block.title"
+              :size="3"
+              :title="block.title"
+              :img="block.image"
+              :link="block.link"
+          />
+        </v-row>
+
+        <br>
+        <br>
+        <br>
+
+      </v-container>
+    </section>
+  </Layout>
+</template>
+
+<script>
+import data from '@/data/huidverjongend.json';
+import FeedCard from '../components/FeedCard';
+
+export default {
+  components: {
+    FeedCard
+  },
+  data: () => ({
+    data,
+    selectedImage: false
+  })
+}
+</script>
